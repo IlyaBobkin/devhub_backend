ce3c8002f97866268d2f7d32dde1fda5b469c614
Илья Бобкин <103562689+IlyaBobkin@users.noreply.github.com>
2025-05-18 17:24:54 +0300

    Update init.js

--- backend/keycloak/init.js
+++ backend/keycloak/init.js
@@ -5,7 +5,7 @@ import session from 'express-session';
 export default async function initKeycloak() {
     const kcConnectConfig = {
         clientId: 'frontend',
-        serverUrl: 'http://keycloak:8080',
+        serverUrl: process.env.KEYCLOAK_URL || 'http://keycloak-srv:8080', // Use env var or internal URL,
         realm: 'hh_realm',
         sslRequired: 'none',
         resource: 'frontend',
@@ -18,7 +18,7 @@ export default async function initKeycloak() {
     const keycloak = new Keycloak({ store: memoryStore }, kcConnectConfig);
 
     const kcAdminClient = new KcAdminClient({
-        baseUrl: 'http://keycloak:8080',
+        baseUrl: process.env.KEYCLOAK_URL || 'http://keycloak-srv:8080',
         realmName: 'hh_realm',
     });
 
